cd1e67aecff37e7e2bd7412d51e8f89c45e58a41
Yoncity <yonatan.dawit.yd2@gmail.com>
2021-12-21 00:29:09 +0200

    added dummy header and app structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+// import Link from "next/link";
+import Image from "next/image";
+import style from "./index.module.scss";
+
+const Header: React.FC<{}> = () => {
+  return (
+    <div className={style.header_container}>
+      <div className={style.header_container__left}>
+        <Image
+          src="/assets/icons/casa.png"
+          alt="Casa icon"
+          width={56}
+          height={56}
+        />
+        <a href="#" className={`${style.header_container__left__app_name}   `}>
+          CASA
+        </a>
+      </div>
+
+      <div className={style.header_container__right}>
+        <a href="#" className={`${style.link} ${style.active}`}>
+          Home
+        </a>
+        <a href="#" className={style.link}>
+          How it Works?
+        </a>
+        <a href="#" className={style.link}>
+          Dashboard
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
+// className={style.header_container__right__link}
